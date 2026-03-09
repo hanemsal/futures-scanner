d452ec52ea31975f3551fc5014b0023999894aa3
hanemsal <hakan.emsal@gmail.com>
2026-03-09 03:40:05 +0300

    Create notify.py

---
+++ notify.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, json=payload)
